0af2c629d4d797c7d8c7ddef5d9bf8423e90ca77
Wooseok Oh <wooseok.oh@naver.com>
2021-11-17 13:26:35 +0900

    minor change

--- Load.py
+++ Load.py
@@ -9,20 +9,18 @@ root.withdraw()
 files = filedialog.askopenfilenames(initialdir=".",
                                     title="파일을 선택 해 주세요",
                                     filetypes=(("*.xlsx", "*xlsx"), ("*.xls", "*xls")))
-# files 변수에 선택 파일 경로 넣기
-
 if files == '':
-    messagebox.showwarning("경고", "파일을 추가 하세요")  # 파일 선택 안했을 때 메세지 출력
+    messagebox.showwarning("경고", "파일을 추가 하세요")
 
-print("Selected file name : ", files[0])  # files 리스트 값 출력
+print("Selected file name : ", files[0])
 
-print("Do you want to add Delimiter between each item (Y/n): ")  # files 리스트 값 출력
+print("Do you want to add Delimiter between each item (Y/n): ")
 x = input()
 
-delstr = '|'
+delim = '|'
 
 if x == 'n' or x == 'N':
-    delstr = ''
+    delim = ''
 
 selectpath = os.path.dirname(files[0])
 selectfile = os.path.basename(files[0])
@@ -53,48 +51,48 @@ for i in range(1, len(data_pd)):
     date = data_pd[21][i]
     datefield2 = date.strftime('%Y%m%d')
 
-    targetText = ('{0:<5}'.format(data_pd[0][i]) +  # 보고서코드
-                  delstr +
-                  '{0:<7}'.format(data_pd[1][i]) +  # 회사코드
-                  delstr +
-                  '{0:<12}'.format(data_pd[2][i]) +  # 펀드코드
-                  delstr +
+    targetText = ('{0:<5}'.format(data_pd[0][i]) +
+                  delim +
+                  '{0:<7}'.format(data_pd[1][i]) +
+                  delim +
+                  '{0:<12}'.format(data_pd[2][i]) +
+                  delim +
                   datefield1 +
-                  delstr +
+                  delim +
                   '{0:<100}'.format(data_pd[4][i]) +
-                  delstr +
+                  delim +
                   '{0:<100}'.format(data_pd[5][i]) +
-                  delstr +
+                  delim +
                   '{0:<1}'.format(data_pd[6][i]) +
-                  delstr +
+                  delim +
                   '{0:<100}'.format(data_pd[7][i]) +
-                  delstr +
+                  delim +
                   '{0:<1}'.format(data_pd[8][i]) +
-                  delstr +
+                  delim +
                   '{0:<1}'.format(data_pd[9][i]) +
-                  delstr +
+                  delim +
                   '{0:<30}'.format(data_pd[10][i]) +
-                  delstr +
+                  delim +
                   '{0:<1}'.format(data_pd[11][i]) +
-                  delstr +
+                  delim +
                   '{0:<50}'.format(data_pd[12][i]) +
-                  delstr +
+                  delim +
                   '{0:<100}'.format(data_pd[13][i]) +
-                  delstr +
+                  delim +
                   '{0:<10.2f}'.format(data_pd[14][i]) +
-                  delstr +
+                  delim +
                   '{0:<3}'.format(data_pd[15][i]) +
-                  delstr +
+                  delim +
                   '{0:<30.2f}'.format(data_pd[16][i]) +
-                  delstr +
+                  delim +
                   '{0:<30.2f}'.format(data_pd[17][i]) +
-                  delstr +
+                  delim +
                   '{0:<30.2f}'.format(data_pd[18][i]) +
-                  delstr +
+                  delim +
                   '{0:<10.1f}'.format(data_pd[19][i]) +
-                  delstr +
+                  delim +
                   '{0:<20}'.format(data_pd[20][i]) +
-                  delstr +
+                  delim +
                   datefield2 +
                   '\n'
                   )
